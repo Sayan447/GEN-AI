4a1687f9695a6f309cd4868eb148288023bc2b0b
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2026-05-06 23:27:26 +0530

    all updates

---
+++ Assignment_05/modules_assignment/main.py
@@ -0,0 +1,9 @@
+# Method 1
+import math_utils
+
+# Method 2
+from math_utils import square
+
+print("Addition:", math_utils.add(5, 3))
+print("Subtraction:", math_utils.subtract(10, 4))
+print("Square:", square(6))
